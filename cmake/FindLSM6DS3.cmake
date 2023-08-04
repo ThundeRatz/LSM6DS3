cd1da09d0aa8e460cf525e5584ecdaab6be70674
pedroazeredo04 <MY_NAME@example.com>
2023-08-04 12:23:41 -0300

    💥 Add CMake

---
+++ cmake/FindLSM6DS3.cmake
@@ -0,0 +1,11 @@
+set(LSM6DS3_PATH .lib/LSM6DS3)
+
+list(APPEND LIB_SOURCES
+    ${LSM6DS3_PATH}/src/lsm6dso_reg.c
+    ${LSM6DS3_PATH}/src/lsm6ds3_reg.c
+    ${LSM6DS3_PATH}/src/lsm6ds.c
+)
+
+list(APPEND LIB_SOURCES
+    ${LSM6DS3_PATH}/inc
+)
