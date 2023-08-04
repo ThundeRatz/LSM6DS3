f3897e231975ec41f8be1e5562da80b0e0b641cf
pedroazeredo04 <MY_NAME@example.com>
2023-08-04 13:12:44 -0300

    🐛 Fix path

--- cmake/FindLSM6DS3.cmake
+++ cmake/FindLSM6DS3.cmake
@@ -1,4 +1,4 @@
-set(LSM6DS3_PATH .lib/LSM6DS3)
+set(LSM6DS3_PATH ./lib/LSM6DS3)
 
 list(APPEND LIB_SOURCES
     ${LSM6DS3_PATH}/src/lsm6dso_reg.c
@@ -6,6 +6,6 @@ list(APPEND LIB_SOURCES
     ${LSM6DS3_PATH}/src/lsm6ds.c
 )
 
-list(APPEND LIB_SOURCES
+list(APPEND LIB_INCLUDE_DIRECTORIES
     ${LSM6DS3_PATH}/inc
 )
