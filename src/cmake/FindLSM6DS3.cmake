f5742a16d6009fcd211200855ce9ad5ea28a31b2
Bruno1406 <bruno.lossmachado@gmail.com>
2023-08-13 18:37:34 -0300

    ⬆️ Cmake Migration

---
+++ src/cmake/FindLSM6DS3.cmake
@@ -0,0 +1,10 @@
+set(LSM6DS3_PATH ./lib/LSM6DS3)
+
+list(APPEND LIB_SOURCES
+    ${LSM6DS3_PATH}/src/lsm6ds3_reg.c
+    ${LSM6DS3_PATH}/src/lsm6ds3.c
+)
+
+list(APPEND LIB_INCLUDE_DIRECTORIES
+    ${LSM6DS3_PATH}/inc
+)
