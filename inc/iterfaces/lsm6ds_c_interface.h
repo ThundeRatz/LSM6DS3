663416377292185a7ef7a02da015fc1c468df9ef
Bruno1406 <bruno.lossmachado@gmail.com>
2023-08-26 19:47:09 -0300

    🎨  Changes C interface function names

--- inc/iterfaces/lsm6ds_c_interface.h
+++ inc/iterfaces/lsm6ds_c_interface.h
@@ -26,7 +26,7 @@ extern "C" {
  * @param  I2C_pinout_config  Struct of sensor pinout configurations in I2C mode 
  *
  */
-int8_t init_I2C(lsm6ds_settings_t lsm6ds_settings, lsm6ds_I2C_pinout I2C_pinout_config);
+int8_t lsm6ds_init_I2C(lsm6ds_settings_t lsm6ds_settings, lsm6ds_I2C_pinout I2C_pinout_config);
 
 /**
  * @brief  Initialize the sensor and set acc/gyro sensitivities and data rates
@@ -35,37 +35,37 @@ int8_t init_I2C(lsm6ds_settings_t lsm6ds_settings, lsm6ds_I2C_pinout I2C_pinout_
  * @param  SPI_pinout_config  Struct of sensor pinout configurations in SPI mode 
  *
  */
-int8_t init_SPI(lsm6ds_settings_t lsm6ds_settings, lsm6ds_SPI_pinout SPI_pinout_config);
+int8_t lsm6ds_init_SPI(lsm6ds_settings_t lsm6ds_settings, lsm6ds_SPI_pinout SPI_pinout_config);
 
 /**
  * @brief Update sensor data if available (Use this function if not using interrupt pins)
  */
-void update_data();
+void lsm6ds_update_data();
 
 /**
  * @brief Update sensor data (Use this function when using interrupt pins)
  */
-void update_data_ready_interrupt();
+void lsm6ds_update_data_ready_interrupt();
 
 /**
  * @brief Update sensor data (Use this function if using fifo continuous mode with interrupt pins)
  */
-void update_data_fifo_full_interrupt();
+void lsm6ds_update_data_fifo_full_interrupt();
 
 /**
  * @brief Resets fifo data
  */
-void reset_fifo();
+void lsm6ds_reset_fifo();
 
 /**
  * @brief Get accelerometer data array in mg
  */
-float* get_acc_data_mg();
+float* lsm6ds_get_acc_data_mg();
 
 /**
  * @brief Get gyroscope data array in mdps
  */
-float* get_gyro_data_mdps();
+float* lsm6ds_get_gyro_data_mdps();
 
 #ifdef __cplusplus
 }
--- src/interfaces/lsm6ds_c_interface.cpp
+++ src/interfaces/lsm6ds_c_interface.cpp
@@ -16,36 +16,36 @@ extern "C" {
 
 static LSM6DS_Interface sensor_interface;
 
-int8_t init_I2C(lsm6ds_settings_t lsm6ds_settings, lsm6ds_I2C_pinout I2C_pinout_config) {
+int8_t lsm6ds_init_I2C(lsm6ds_settings_t lsm6ds_settings, lsm6ds_I2C_pinout I2C_pinout_config) {
     return sensor_interface.init(lsm6ds_settings, I2C_pinout_config);
 }
 
-int8_t init_SPI(lsm6ds_settings_t lsm6ds_settings, lsm6ds_SPI_pinout SPI_pinout_config) {
+int8_t lsm6ds_init_SPI(lsm6ds_settings_t lsm6ds_settings, lsm6ds_SPI_pinout SPI_pinout_config) {
     return sensor_interface.init(lsm6ds_settings, SPI_pinout_config);
 }
 
-void update_data() {
+void lsm6ds_update_data() {
     return sensor_interface.update_data();
 }
 
 
-void LSM6DS_Interface::update_data_ready_interrupt() {
+void lsm6ds_update_data_ready_interrupt() {
     sensor_interface.update_data_ready_interrupt();
 }
 
-void LSM6DS_Interface::update_data_fifo_full_interrupt() {
+void lsm6ds_update_data_fifo_full_interrupt() {
     sensor_interface.update_data_fifo_full_interrupt();
 }
 
-void LSM6DS_Interface::reset_fifo() {
+void lsm6ds_reset_fifo() {
     sensor_interface.reset_fifo();
 }
 
-float* LSM6DS_Interface::get_acc_data_mg() {
+float* lsm6ds_get_acc_data_mg() {
     return sensor_interface.get_acc_data_mg();
 }
 
-float* LSM6DS_Interface::get_gyro_data_mdps() {
+float* lsm6ds_get_gyro_data_mdps() {
     return sensor_interface.get_gyro_data_mdps();
 }
 
